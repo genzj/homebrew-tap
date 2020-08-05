77e07f67a7a5e8e76172d374a80b96f6cd6b713a
Jie Zhu <zhuje@amazon.com>
2020-08-05 13:13:56 -0700

    [goTApaper] pass brew audit

--- gotapaper.rb
+++ gotapaper.rb
@@ -1,31 +1,26 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Gotapaper < Formula
-  desc "Download latest pictures from many providers and use them as wallpaper."
+  desc "Download wallpaper from various sources"
   homepage "https://github.com/genzj/goTApaper"
-  head "https://github.com/genzj/goTApaper.git"
   url "https://github.com/genzj/goTApaper/archive/v0.3.2.tar.gz"
   sha256 "267a3df431b951253056d5c908151ed0f4cc513675783fb29b479964c66afc8e"
   license "MIT"
+  head "https://github.com/genzj/goTApaper.git"
 
-  @@commit="fd35fee7f9f4e4dd1efb6bd10e5be3f1a473503f"
+  GIT_COMMIT_ID="fd35fee7f9f4e4dd1efb6bd10e5be3f1a473503f".freeze
 
   depends_on "go" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
     if ENV.key?("HOMEBREW_OVERRIDE_GOPROXY")
+      # to support installation from inner network
       ENV["GOPROXY"] = ENV["HOMEBREW_OVERRIDE_GOPROXY"]
     end
 
-    Dir.chdir("#{buildpath}/generate") {
-      system "go", "generate", "-v", "-x"
-    }
-    system "go", "build", "-v", "-x", "-ldflags", "-X github.com/genzj/goTApaper/cmd.Version=#{version} -X github.com/genzj/goTApaper/cmd.GitCommit=#{@@commit} -X github.com/genzj/goTApaper/cmd.VersionPrerelease=HomeBrew", *std_go_args
+    Dir.chdir("#{buildpath}/generate") { system "go", "generate", "-v", "-x" }
+    system "go", "build", "-ldflags", "-X github.com/genzj/goTApaper/cmd.Version=#{version} -X github.com/genzj/goTApaper/cmd.GitCommit=#{GIT_COMMIT_ID} -X github.com/genzj/goTApaper/cmd.VersionPrerelease=HomeBrew", *std_go_args
   end
 
   test do
-    assert_equal "goTApaper - #{version} HomeBrew(#{@@commit})", shell_output("#{bin}/goTApaper version").strip
+    assert_equal "goTApaper - #{version} HomeBrew(#{GIT_COMMIT_ID})", shell_output("#{bin}/goTApaper version").strip
   end
 end
