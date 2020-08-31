b6493f2500040e9fba80d463077143ec60f44821
Jie Zhu <zhuje@amazon.com>
2020-08-31 15:42:51 -0700

    bump goTApaper to v0.4.1

--- gotapaper.rb
+++ gotapaper.rb
@@ -1,12 +1,12 @@
 class Gotapaper < Formula
   desc "Download wallpaper from various sources"
   homepage "https://github.com/genzj/goTApaper"
-  url "https://github.com/genzj/goTApaper/archive/v0.3.2.tar.gz"
-  sha256 "267a3df431b951253056d5c908151ed0f4cc513675783fb29b479964c66afc8e"
+  url "https://github.com/genzj/goTApaper/archive/v0.4.1.tar.gz"
+  sha256 "d7b33717ca60e1cc269a4fa4b3713d131b8a5d746570da12ce2161ac8d6fa1cc"
   license "MIT"
   head "https://github.com/genzj/goTApaper.git"
 
-  GIT_COMMIT_ID="fd35fee7f9f4e4dd1efb6bd10e5be3f1a473503f".freeze
+  GIT_COMMIT_ID="65db40093f962bb6261a4a803b038ae8b59ff196".freeze
 
   depends_on "go" => :build
 
